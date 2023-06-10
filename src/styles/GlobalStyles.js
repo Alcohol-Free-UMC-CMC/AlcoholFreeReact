b58c60b7556f7a60a6f2e8f0e0d0f141eb0984b8
HoCG <rhwkd5012@naver.com>
2023-06-10 16:48:28 +0900

    feat: 글로벌 스타일 적용

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+import { reset } from "styled-reset";
+
+export const GlobalStyles = createGlobalStyle`
+  ${reset}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "GmarketSans", Arial, Helvetica, sans-serif
+  }
+  
+  body {
+    max-width: 500px;
+    font-family: "GmarketSans", Arial, Helvetica, sans-serif
+  }
+  
+  a {
+    text-decoration: none;
+  }
+
+  input:focus, textarea:focus {
+    outline: none;
+  }
+`;
