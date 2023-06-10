08e2c23374562a06ad4faaf580659c5c7c726969
HoCG <rhwkd5012@naver.com>
2023-06-10 17:51:51 +0900

    feat: 로고버튼에 라우팅 반영 및 메뉴버튼 스타일 수정

--- src/components/layouts/Menus/MenuButton/MenuButton.style.js
+++ src/components/layouts/Menus/MenuButton/MenuButton.style.js
@@ -4,6 +4,9 @@ const MenuButtonContainer = styled.button`
   margin: 10px;
   font-size: 1.3rem;
   font-weight: 800;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 export { MenuButtonContainer }
--- src/components/layouts/header/LogoButton/LogoButton.jsx
+++ src/components/layouts/header/LogoButton/LogoButton.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { LogoContainer } from "./LogoButton.style";
+import { useNavigate } from "react-router-dom";
 
 const LogoButton = () => {
+  const navigate = useNavigate();
   return (
-    <LogoContainer>
+    <LogoContainer onClick={() => navigate("/")}>
       
     </LogoContainer>
   );
