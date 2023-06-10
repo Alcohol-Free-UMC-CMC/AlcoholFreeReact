5ca7a8bce4945adc43432710c5d8361007f70476
HoCG <rhwkd5012@naver.com>
2023-06-10 20:43:29 +0900

    feat: Recipe 카드 구현

---
+++ src/components/recipe/RecipeCard/RecipeCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RecipeCardContainer } from "./RecipeCard.style";
+
+const RecipeCard = () => {
+  return (
+    <RecipeCardContainer>
+      
+    </RecipeCardContainer>
+  );
+};
+
+export default RecipeCard;
---
+++ src/components/recipe/RecipeCard/RecipeCard.style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const RecipeCardContainer = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 27px;
+  background-color: #D9D9D9;
+  cursor: pointer;
+`;
+
+export { RecipeCardContainer }
